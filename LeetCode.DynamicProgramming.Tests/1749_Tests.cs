18aa7db417ece5ad4ac4a05b49d64125e9fdbe77
Kolia Vaskevych <kolyavaskevich19@gmail.com>
2022-06-18 13:26:38 +0300

    Add solution for 1749

---
+++ LeetCode.DynamicProgramming.Tests/1749_Tests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.DynamicProgramming._1749;
+using NUnit.Framework;
+
+namespace LeetCode.DynamicProgramming.Tests;
+
+[TestFixture]
+public class _1749_Tests {
+
+    [Test]
+    [TestCase(new[]{1,-3,2,3,-4}, 5)]
+    [TestCase(new[]{2,-5,1,-4,3,-2}, 8)]
+    public void MaxAbsoluteSum_CorrectAnswer(int[] arr, int expected)
+    {
+        var solution = new Solution();
+
+        var res = solution.MaxAbsoluteSum(arr);
+        
+        Assert.True(res == expected);
+    }
+}
---
+++ LeetCode.DynamicProgramming/1749/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.DynamicProgramming._1749;
+
+public class Solution
+{
+    public int MaxAbsoluteSum(int[] A) {
+        int s = 0, min = 0, max = 0;
+        foreach (var a in A) {
+            s += a;
+            min = Math.Min(min, s);
+            max = Math.Max(max, s);
+        }
+        return max - min;
+    }
+}
