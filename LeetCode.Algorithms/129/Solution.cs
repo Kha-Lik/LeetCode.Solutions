94204012fcc4e9f1963b2bba8e12ab38a78cfd72
Kolia Vaskevych <kolyavaskevich19@gmail.com>
2022-06-18 13:24:23 +0300

    Add solution for 129

---
+++ LeetCode.Algorithms/129/Solution.cs
@@ -0,0 +1,35 @@
+﻿#nullable disable
+namespace LeetCode.Algorithms._129;
+
+public class Solution {
+    public int SumNumbers(TreeNode root)
+    {
+        return SumNumbers(root, 0);
+    }
+
+    private int SumNumbers(TreeNode root, int currentSum)
+    {
+        if (root is null) return 0;
+
+        currentSum = currentSum * 10 + root.val;
+
+        if (root.left is null && root.right is null)
+            return currentSum;
+
+        return SumNumbers(root.left, currentSum) + SumNumbers(root.right, currentSum);
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
