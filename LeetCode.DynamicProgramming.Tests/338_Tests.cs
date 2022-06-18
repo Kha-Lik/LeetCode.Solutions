e665ee79f6beb686aafa4d72a7f862dbe2becc2d
Kolia Vaskevych <kolyavaskevich19@gmail.com>
2022-06-18 13:26:30 +0300

    Add solution for 338

---
+++ LeetCode.DynamicProgramming.Tests/338_Tests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.DynamicProgramming._338;
+using NUnit.Framework;
+
+namespace LeetCode.DynamicProgramming.Tests;
+
+public class _338_Tests {
+    
+    [Test]
+    [TestCase(0, new[]{0})]
+    [TestCase(1, new[]{0, 1})]
+    [TestCase(2, new[]{0, 1, 1})]
+    [TestCase(5, new[]{0, 1, 1, 2, 1, 2})]
+    [TestCase(12, new[]{0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2})]
+    public void CountBits_LinearSolution_CorrectResult(int n, int[] expected)
+    {
+        var solution = new Solution();
+
+        var result = solution.CountBits(n);
+        
+        Assert.AreEqual(result, expected);
+    }
+}
---
+++ LeetCode.DynamicProgramming/338/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.DynamicProgramming._338;
+
+public class Solution {
+    public int[] CountBits(int n) {
+        var arr = new int[n+1];
+        for(int i = 1; i<=n; i++){
+            arr[i] = i%2 is 0 ? arr[i/2] : arr[i/2]+1;
+        }
+        return arr;
+    }
+}
