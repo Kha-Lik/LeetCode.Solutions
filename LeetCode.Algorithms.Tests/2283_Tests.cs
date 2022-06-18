04715942d58b19e55c3cf5838debee36826e25bb
Kolia Vaskevych <kolyavaskevich19@gmail.com>
2022-06-18 13:25:58 +0300

    Add solution for 2283

---
+++ LeetCode.Algorithms.Tests/2283_Tests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Algorithms._2283;
+using NUnit.Framework;
+
+namespace LeetCode.Algorithms.Tests;
+
+[TestFixture]
+public class _2283_Tests {
+
+    [Test]
+    [TestCase("1210", true)]
+    public void DigitCount_CorrectAnswer(string num, bool expected)
+    {
+        var solution = new Solution();
+
+        var res = solution.DigitCount(num);
+        
+        Assert.That(res == expected);
+    }
+}
---
+++ LeetCode.Algorithms/2283/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms._2283;
+
+public class Solution
+{
+    public bool DigitCount(string num)
+    {
+        var dict = new Dictionary<char, int>();
+        foreach (var c in num)
+        {
+            if (!dict.ContainsKey(c)) dict[c] = 1;
+            else dict[c]++;
+        }
+        
+        for (var i = 0; i < num.Length; i++)
+        {
+            var digit = (char) (i + '0');
+            var count = num[i] - '0';
+            if (!(dict.ContainsKey(digit)))
+                if (count == 0) continue;
+                else return false;
+            if(dict[digit] != count)
+                return false;
+        }
+
+        return true;
+    }
+}
