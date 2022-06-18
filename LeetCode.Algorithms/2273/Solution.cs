c3b37ab295efe3d00f9b0fe5a595297fa8e88f53
Kolia Vaskevych <kolyavaskevich19@gmail.com>
2022-06-18 13:25:44 +0300

    Add solution for 2273

---
+++ LeetCode.Algorithms/2273/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms._2273;
+
+public class Solution {
+    public IList<string> RemoveAnagrams(string[] words)
+    {
+        var stack = new Stack<string>();
+        for (int i = words.Length - 1; i >= 0; i--)
+        {
+            string s = words[i];
+            while (stack.Count != 0 && AreAnagrams(stack.Peek(), s)) 
+                stack.Pop();
+            stack.Push(s);
+        }
+
+        var res = new List<string>();
+        while (stack.Count != 0) 
+            res.Add(stack.Pop());
+        return res;
+    }
+
+    private static bool AreAnagrams(string str1, string str2)
+    {
+        if (str1.Length != str2.Length)
+            return false;
+
+        var dict = new Dictionary<char, int>();
+        
+        foreach (var c in str1)
+        {
+            if (dict.ContainsKey(c))
+                dict[c]++;
+            else
+                dict[c] = 1;
+        }
+        
+        foreach (var c in str2)
+        {
+            if (dict.ContainsKey(c))
+                dict[c]--;
+        }
+        
+        foreach (var dictKey in dict.Keys)
+        {
+            if (dict[dictKey] != 0) return false;
+        }
+
+        return true;
+    }
+}
