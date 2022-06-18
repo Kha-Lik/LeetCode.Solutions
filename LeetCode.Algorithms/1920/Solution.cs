d54644e82bd67dafb827ebd2cc728232899b4398
Kolia Vaskevych <kolyavaskevich19@gmail.com>
2022-06-18 13:25:27 +0300

    Add solution for 1920

---
+++ LeetCode.Algorithms/1920/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Algorithms._1920;
+
+public class Solution
+{
+    public int[] BuildArray(int[] nums)
+    {
+        var newNums = new int[nums.Length];
+        int i = 0;
+        foreach (var num in nums)
+        {
+            newNums[i] = nums[num];
+            i++;
+        }
+
+        return newNums;
+    }
+}
