d035fac6d77f2f080167bdd392ad03e14d5f850b
Kolia Vaskevych <kolyavaskevich19@gmail.com>
2022-06-18 13:24:41 +0300

    Add solution for 815

---
+++ LeetCode.Algorithms/815/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Algorithms._815;
+
+public class Solution {
+    public int NumBusesToDestination(int[][] routes, int source, int target)
+    {
+        var graph = new Dictionary<int, HashSet<int>>();
+
+        for (var i = 0; i < routes.GetLength(0); i++)
+        {
+            var stops = routes[i];
+            for (var j = 0; j < stops.Length; j++)
+            {
+                var stop = stops[j];
+                if (!graph.ContainsKey(stop)) 
+                    graph[stop] = new HashSet<int>();
+                graph[stop].UnionWith(stops.Where(s => s != stop));
+            }
+        }
+        return -1;
+    }
+}
