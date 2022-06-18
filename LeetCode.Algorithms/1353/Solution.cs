b19ebd2ae352209354db6bd09deab8fe97ec8101
Kolia Vaskevych <kolyavaskevich19@gmail.com>
2022-06-18 13:25:00 +0300

    Add solution for 1353

---
+++ LeetCode.Algorithms/1353/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace LeetCode.Algorithms._1353;
+
+public class Solution {
+    public int MaxEvents(int[][] events)
+    {
+        var min = events.Select(x => x[0]).Min();
+        var counter = 1;
+        var current = events.First(e => e[0] == 1);
+        while (true)
+        {
+            current = events.FirstOrDefault(e => e[0] == current[1]);
+            if (current is not null)
+                counter++;
+            else
+                break;
+            
+        }
+
+        return counter;
+    }
+}
