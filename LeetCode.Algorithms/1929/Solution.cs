4012597c1e0a0fe27e0d263029385d8e9aa6788e
Kolia Vaskevych <kolyavaskevich19@gmail.com>
2022-06-18 13:25:37 +0300

    Add solution for 1929

---
+++ LeetCode.Algorithms/1929/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Algorithms._1929;
+
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        var arr = new int[nums.Length * 2];
+        nums.CopyTo(arr, 0);
+        nums.CopyTo(arr, nums.Length);
+        return arr;
+    }
+}
